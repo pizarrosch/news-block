e192b7216b43b46f0dc1da4333451c3fa6cf288e
Zaur <bayernsch89@gmail.com>
2022-05-01 18:59:02 +0200

    Add filter to genres

--- js/SubcategoryData.js
+++ js/SubcategoryData.js
@@ -1,5 +1,6 @@
 function createNewBlock(subdivision) {
   const div = create('div');
+  div.classList.add('book');
   const p = create('p');
   const a = create('a');
 
@@ -7,6 +8,7 @@ function createNewBlock(subdivision) {
 
   p.textContent = subdivision.title;
   div.value = subdivision.category;
+  div.dataset.category = subdivision.genre;
 
   div.append(p);
 
@@ -17,6 +19,20 @@ const subcategories = $('.subcategories');
 
 for (const subdivision of subcategoryData) {
   createNewBlock(subdivision);
+}
+
+const divs = $$('.book');
+const filter = $('.select');
+
+
+filter.onchange = () => {
+  for (const div of divs) {
+  if(div.dataset.category !== filter.value && filter.value !== 'all-genres') {
+    div.classList.add('hidden');
+  } else {
+    div.classList.remove('hidden');
+  }
+ }
 }
 
 
